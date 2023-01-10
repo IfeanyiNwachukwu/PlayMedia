5a8aab287853c419391244f04637c280cb835296
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-01-10 01:07:38 +0100

    Implementing Mediator Pattern

---
+++ SM-POST/Post.cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Infrastructure;
+using CQRS.Core.Messages;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly Dictionary<Type,Func<BaseCommand, Task>> _handlers = new();
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if(_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("You cannot register the same command handler twice!");
+            }
+            _handlers.Add(typeof(T), x => handler((T)x));
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if(_handlers.TryGetValue(command.GetType(), out Func<BaseCommand,Task>handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No Command handler was registered!");
+            }
+        }
+    }
+}
